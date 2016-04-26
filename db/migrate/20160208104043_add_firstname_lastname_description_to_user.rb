4dd985309a502fcb3e126dc534db8851b589f32c
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2016-04-26 21:50:57 +0300

    deleted files

--- db/migrate/20160208104043_add_firstname_lastname_description_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddFirstnameLastnameDescriptionToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :firstname, :string
-    add_column :users, :lastname, :string
-    add_column :users, :description, :text
-  end
-end
